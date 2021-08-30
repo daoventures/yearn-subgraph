b43686284a2bc3eac65ff6547b4d31f4bfea906e
Phyllice Wai <waikahkwan0903@gmail.com>
2021-08-30 17:43:35 +0800

    Uni Bot: Add mapping for Uni Bot

--- src/map/dvdBotMapping.ts
+++ src/map/dvdBotMapping.ts
@@ -1,4 +1,5 @@
 import { DistDVD } from "../../generated/DVDDistBot/DVDDistributionBot";
+import { BuyDVD } from "../../generated/DVDUniBot/DVDUniBot";
 import { BuyBack, Cumulative } from "../../generated/schema";
 import { BIGINT_ZERO } from "../utils/constants";
 
@@ -31,6 +32,28 @@ function getOrCreateCumulativeBuyBack(
 
 // DVD Distribution Bot
 export function handleDvdDistributionBuyBack(event: DistDVD): void {
+    let transactionId = event.transaction.hash.toHexString();
+
+    let buyBack = getOrCreateBuyBack(transactionId);
+    buyBack.from = event.params.user.toHexString();
+    buyBack.to = event.transaction.to.toHexString();
+    buyBack.transactionHash = event.transaction.hash;
+    buyBack.blockNumber = event.block.number;
+    buyBack.amountRaw = event.params.dvdAmount;
+    buyBack.timestamp = event.block.timestamp;
+    buyBack.save();
+
+    let cumulativeBuyBack = getOrCreateCumulativeBuyBack(event.transaction.to.toHexString());
+
+    let cumulativeBuyBackAmount = cumulativeBuyBack.totalAmountRaw;
+    cumulativeBuyBackAmount = cumulativeBuyBackAmount.plus(buyBack.amountRaw);
+    cumulativeBuyBack.totalAmountRaw = cumulativeBuyBackAmount;
+
+    cumulativeBuyBack.lastUpdate = event.block.timestamp;
+    cumulativeBuyBack.save();
+}
+
+export function handleDvdUniBotBuyBack(event: BuyDVD): void {
     let transactionId = event.transaction.hash.toHexString();
 
     let buyBack = getOrCreateBuyBack(transactionId);
